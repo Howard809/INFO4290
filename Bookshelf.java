1ffc3af5bfaaf3de0854f2dec6ed9f2db1a152a8
cmstu29 <carolyn.mah@student.kpu.ca>
2025-02-09 20:45:28 -0800

    Add files via upload

--- Bookshelf.java
+++ Bookshelf.java
@@ -1,7 +1,7 @@
 /*Bookshelf.java
  *Author: Howard Chen - 100382934
  *Date: February 09, 2025
- *Main Code portion for the Git Exercise - Group - Part 2
+ *Main Code portion for the Git Exercise - Group - Part 1
 */
 
 // Purpose: Design and implement another Java class named BookShelf which has an ArrayList data
@@ -15,43 +15,25 @@
 import java.util.*;
 
 public class Bookshelf {	
-	//key attribute
-	ArrayList<Book> bookList = new ArrayList<>();
-	
-	//default constructor
-	public Bookshelf () { }
-	
-	//constructor with parameters
-	public Bookshelf (ArrayList<Book> list){
-		this.bookList = list;
-	}	
-    
-	//declare and define accessor and mutator for "key" attribute(s)
-	public ArrayList<Book> getList() {       //accessor
-		return bookList;
+
+	public static void main (String[] args) {
+		/* Main Method */
+		Bookshelf shelf = new Bookshelf();
+		Book book1 = new Book("Jojo's Bizarre Adventure", 9);
+		Book book2 = new Book("The Empire Strikes Back", 15);
+		Book book3 = new Book("The Art of War", 15);
+		Book book4 = new Book("Guiness Book of Records", 19);
+		Book book5 = new Book("Diary of a Wimpy Kid", 6);
+		shelf.addBook(book1);
+		shelf.addBook(book2);
+		shelf.addBook(book3);
+		shelf.addBook(book4);
+		shelf.addBook(book5);
+		System.out.println(shelf); //prints the whole shelf
+		Sorting.main(); // sorts the shelf and prints it in reverse order
+		System.out.println("Book 3 has been checked out.");
+		shelf.removeBook(book3); //removes the third book from the shelf
+		System.out.println(shelf); //prints the updated shelf
+		System.out.println("Is book 3, 'The Art of War' available? "+shelf.findBook(book3)); //searches for the third book, should give "false" as the result
 	}
-	
-	public void setList(ArrayList<Book> list) { //mutator
-		this.bookList = list;
-	}
-	
-	//toString method() is called automatically when a book object is printed
-	public String toString() {
-		return this.bookList.toString();
-	}
-	
-	//methods for adding, removing, and searching the bookList
-	public void addBook (Book book) {
-		bookList.add(book);
-	}
-	
-	public void removeBook (Book book) {
-		bookList.remove(book);
-	}
-	
-	public boolean findBook (Book book) {
-		return bookList.contains(book);
-	}
-	
-	
 }
