3dc35771905d7a505f0d866e498dd9c1c810e112
cmstu29 <carolyn.mah@student.kpu.ca>
2025-02-09 20:57:24 -0800

    Add files via upload

--- Bookshelf.java
+++ Bookshelf.java
@@ -1,7 +1,7 @@
 /*Bookshelf.java
  *Author: Howard Chen - 100382934
  *Date: February 09, 2025
- *Main Code portion for the Git Exercise - Group - Part 1
+ *Main Code portion for the Git Exercise - Group - Part 2
 */
 
 // Purpose: Design and implement another Java class named BookShelf which has an ArrayList data
@@ -15,7 +15,44 @@
 import java.util.*;
 
 public class Bookshelf {	
-
+	//key attribute
+	ArrayList<Book> bookList = new ArrayList<>();
+	
+	//default constructor
+	public Bookshelf () { }
+	
+	//constructor with parameters
+	public Bookshelf (ArrayList<Book> list){
+		this.bookList = list;
+	}	
+    
+	//declare and define accessor and mutator for "key" attribute(s)
+	public ArrayList<Book> getList() {       //accessor
+		return bookList;
+	}
+	
+	public void setList(ArrayList<Book> list) { //mutator
+		this.bookList = list;
+	}
+	
+	//toString method() is called automatically when a book object is printed
+	public String toString() {
+		return this.bookList.toString();
+	}
+	
+	//methods for adding, removing, and searching the bookList
+	public void addBook (Book book) {
+		bookList.add(book);
+	}
+	
+	public void removeBook (Book book) {
+		bookList.remove(book);
+	}
+	
+	public boolean findBook (Book book) {
+		return bookList.contains(book);
+	}
+	
 	public static void main (String[] args) {
 		/* Main Method */
 		Bookshelf shelf = new Bookshelf();
